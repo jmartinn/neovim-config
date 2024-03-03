195d3f2e6c7f81d355e269c56158aaf25c55a0a2
Juan Pedro Martin <juamp_m@hotmail.com>
2024-03-03 19:44:45 +0100

    added python language support

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -6,12 +6,15 @@ function M.config()
   local null_ls = require "null-ls"
 
   local formatting = null_ls.builtins.formatting
+  local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup {
     debug = true,
     sources = {
       formatting.stylua,
       formatting.prettier,
+      formatting.black.with { extra_args = { "--fast" } },
+      diagnostics.flake8,
       -- formatting.prettier.with {
       --   extra_filetypes = { "toml" },
       --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
