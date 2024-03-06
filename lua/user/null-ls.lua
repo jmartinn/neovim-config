f98f134e7bb5329b3471c280ce0033d4f3d54edc
Juan Pedro Martin <juamp_m@hotmail.com>
2024-03-06 20:40:37 +0100

    unused built-in diagnostics w flake

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -6,7 +6,7 @@ function M.config()
   local null_ls = require "null-ls"
 
   local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
+  -- local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup {
     debug = true,
@@ -14,7 +14,7 @@ function M.config()
       formatting.stylua,
       formatting.prettier,
       formatting.black.with { extra_args = { "--fast" } },
-      diagnostics.flake8,
+      -- diagnostics.flake8,
       -- formatting.prettier.with {
       --   extra_filetypes = { "toml" },
       --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
