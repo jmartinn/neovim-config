0e89281d91b2166fc6c108a52364f3c09f362486
Juan Pedro Martin <juamp_m@hotmail.com>
2024-03-07 13:09:32 +0100

    fixed obsidian.nvim config file

--- lua/user/extras/obsidian.lua
+++ lua/user/extras/obsidian.lua
@@ -1,31 +1,27 @@
 local M = {
   "epwalsh/obsidian.nvim",
   lazy = true,
+  ft = { "markdown" },
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
 }
 
 function M.config()
+  local home = os.getenv "HOME"
   require("obsidian").setup {
-    ft = "markdown",
-    opts = {
-      workspaces = {
-        {
-          name = "personal",
-          path = "/Users/jmartin/Library/Mobile Documents/iCloud~md~obsidian/Documents/personal",
-        },
+    workspaces = {
+      {
+        name = "personal",
+        path = home .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/personal",
       },
-      completion = {
-        nvim_cmp = true,
-
-        min_chars = 2,
-      },
-      new_notes_location = "current_dir",
-      prepend_note_id = true,
-      prepend_note_path = false,
-      use_path_only = false,
     },
+    completion = {
+      nvim_cmp = true,
+      min_chars = 2,
+    },
+    new_notes_location = "current_dir",
+    log_level = vim.log.levels.INFO,
   }
 end
 
