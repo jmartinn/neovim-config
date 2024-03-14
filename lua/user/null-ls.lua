a1902047b5efd06d3e16af19e32d07dadc9d62b8
Juan Pedro Martin <juamp_m@hotmail.com>
2024-03-14 23:09:43 +0100

    excluded markdown files from formatting config

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -6,21 +6,29 @@ function M.config()
   local null_ls = require "null-ls"
 
   local formatting = null_ls.builtins.formatting
-  -- local diagnostics = null_ls.builtins.diagnostics
-
   null_ls.setup {
     debug = true,
     sources = {
       formatting.stylua,
-      formatting.prettier,
+      formatting.prettier.with {
+        filetypes = {
+          "javascript",
+          "javascriptreact",
+          "typescript",
+          "typescriptreact",
+          "vue",
+          "css",
+          "scss",
+          "less",
+          "html",
+          "json",
+          "jsonc",
+          "yaml",
+          "graphql",
+          "handlebars",
+        },
+      },
       formatting.black.with { extra_args = { "--fast" } },
-      -- diagnostics.flake8,
-      -- formatting.prettier.with {
-      --   extra_filetypes = { "toml" },
-      --   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-      -- },
-      -- formatting.eslint,
-      -- null_ls.builtins.diagnostics.eslint,
       null_ls.builtins.completion.spell,
     },
   }
