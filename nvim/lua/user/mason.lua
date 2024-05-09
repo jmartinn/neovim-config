6ab397cfd3b6d1d2d0c92bddf24410fa58723c4e
jmartinn <juamp_m@hotmail.com>
2024-05-09 04:23:08 +0200

    updated lsp settings

--- nvim/lua/user/mason.lua
+++ nvim/lua/user/mason.lua
@@ -1,30 +1,33 @@
 local M = {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = {
-    "williamboman/mason.nvim",
-  },
+	"williamboman/mason-lspconfig.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
 }
 
-
 function M.config()
-  local servers = {
-    "lua_ls",
-    "cssls",
-    "tsserver",
-    "pyright",
-    "bashls",
-    "jsonls",
-  }
+	local servers = {
+		"lua_ls",
+		"cssls",
+		"html",
+		"tsserver",
+		"eslint",
+		"pyright",
+		"bashls",
+		"jsonls",
+		"yamlls",
+		"cmake",
+	}
 
-  require("mason").setup {
-    ui = {
-      border = "rounded",
-    },
-  }
+	require("mason").setup({
+		ui = {
+			border = "rounded",
+		},
+	})
 
-  require("mason-lspconfig").setup {
-    ensure_installed = servers,
-  }
+	require("mason-lspconfig").setup({
+		ensure_installed = servers,
+	})
 end
 
 return M
